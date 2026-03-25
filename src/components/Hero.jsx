19fce348bf2c8825c7de97e4f6b57add54ca2142
Basant Bhushan <basantbhushan89@gmail.com>
2026-03-25 22:28:47 +0530

    Add src/components/Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Hero() {
+  const tagline = "Your Gateway to React Mastery.";
+  return (
+    <section className="hero">
+      <div className="container">
+        <h1>True React Test</h1>
+        <p>{tagline}</p>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
